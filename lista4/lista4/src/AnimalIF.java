9d1dd2a1f23397697ad95853f4224ea60178b0c1
Pedro Lima <121100935+PPedrinho@users.noreply.github.com>
2024-08-23 11:18:14 -0300

    Delete lista4/lista4 directory

--- lista4/lista4/src/AnimalIF.java
+++
@@ -1,6 +0,0 @@
-public interface AnimalIF {
-    public void comer(int qntdComida);
-    public void moverse(double qntdCaminho);
-    public void dormir(int qntdHorasDormidas);
-
-}
--- lista4/lista4/src/AnimalMarinhoAB.java
+++
@@ -1,7 +0,0 @@
-public class AnimalMarinhoAB extends AnimalAB {
-
-    public AnimalMarinhoAB(String nome, double peso, String tipo, int idade, String habitat, double altura) {
-        super(nome, peso, tipo, idade, habitat, altura);
-    }
-
-}
